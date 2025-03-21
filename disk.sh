b17bde75406bab261640113a44394358f0f87428
Tyziryx <97693277+Tyziryx@users.noreply.github.com>
2025-03-21 13:33:49 +0100

    argjson dans le sh pour qu'il soit pas une chaine de charracters

--- disk.sh
+++ disk.sh
@@ -1,3 +1,3 @@
 disk_usage=$(df -h / | grep '/' | tr -s ' ' | cut -d' ' -f5 | tr -d '%')
-json_output=$(jq -n --arg usage "$disk_usage" '{disk_usage: $usage}') # crée objet en json
+json_output=$(jq -n --argjson usage "$disk_usage" '{disk_usage: $usage}') # crée objet en json
 echo $json_output
